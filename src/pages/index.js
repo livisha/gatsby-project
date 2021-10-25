a9b017de46799bcc4897e0e48bf403d41464f35f
Livisha jain <61044662+livisha@users.noreply.github.com>
2021-10-25 18:32:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,10 @@ import Image from "../components/image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+// IndexPage is a functional component calling sub-coponent like
+// Layout provides structure to the website by having header, footer and other sectional components
+// Seo is a  component which provide content to provide hints to the search engine crawlers to easily understand your website.
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
