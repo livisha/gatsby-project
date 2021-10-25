c94aa2ec3efb3081df93f5dc3a3a3b3d0f767d74
Livisha jain <61044662+livisha@users.noreply.github.com>
2021-10-25 18:33:51 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,12 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+// 404.js is a error message fie whenver there is a error or crash of web 
+// page the content of this file is rendered.
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
+     {/* NotFoundPage is a functional component containing <Layout>, <Seo> components to present message. */}
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
