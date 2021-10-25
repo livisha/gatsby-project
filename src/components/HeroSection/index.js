1cb3f406a9ab49404b67d8ae9f51c78e5de536ec
Livisha jain <61044662+livisha@users.noreply.github.com>
2021-10-25 18:40:57 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Button } from '../ButtonElements';
 import './HeroSection.css'
 
+// HeroSection is a function that behaves as a home page i.e. contains the basic tags and information.
 function HeroSection(){
     return (
         <div className="hero-container">
         <h1>buy More</h1>
         <p>Plan to next buy today</p>
         <div className="hero-btns">
+             {/* This is a button component that passes parameters and that parameter values are used in further process. */}
             <Button fontBig big primary>Get Started</Button>
         </div>
         </div>                                     
