d032f83d4f970362312601fb0ac8dde39ad52cc8
Livisha jain <61044662+livisha@users.noreply.github.com>
2021-10-25 18:33:15 +0530

    Update destination.js

--- src/pages/destination.js
+++ src/pages/destination.js
@@ -2,6 +2,9 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../../src/components/layout.css"
+
+// destination.js is a arrow functional component containing <Layout>, 
+// <Seo> components to present the Thank you message in the pricing menu.
 const Pricing= () => (
   <Layout>
     <Seo title="Contact me" />
